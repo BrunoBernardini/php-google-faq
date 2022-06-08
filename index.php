26baf36efe39fa2940e59016da4bd7238fa12e83
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-06-08 22:08:07 +0200

    Affinamenti.

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php ?>
-
 <?php
 
 /*
@@ -9,7 +7,6 @@
   Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
   Per la formattazione del testo della FAQ inserite il markup HTML direttamente nel “database”
   buon lavoro!
-
   
  */
 
@@ -59,7 +56,7 @@ $privacyTerms = [
     "title" => "<h2>Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?</h2>",
     "paragraph" => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>"
   ]
-]
+];
 ?>
 
 <!DOCTYPE html>
